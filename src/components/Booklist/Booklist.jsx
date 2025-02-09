2c3a92579d5550810d33ae4b85ca1a78de8c14f7
alshahrear <alshahrear777@gmail.com>
2025-02-09 15:22:01 +0800

    book list complete

--- src/components/Booklist/Booklist.jsx
+++ src/components/Booklist/Booklist.jsx
@@ -2,31 +2,34 @@ import { FaRegStar } from "react-icons/fa";
 
 const BookList = ({booklist}) => {
 
-    const { bookId, bookName, author, image, review, totalPages, rating, category, tags, publisher, yearOfPublishing } = booklist;
+    const { bookId, bookName, author, image, review, totalPages, rating, category, tags1, tags2, publisher, yearOfPublishing } = booklist;
 
     return (
         <div className="">
-            <div>
-                <div className="card bg-base-100 w-96 shadow-xl">
-                    <figure>
+            <div className="">
+                <div className=" p-5 card rounded-xl border-2 border-gray-200 ">
+                    <div className="">
+                    <figure className="bg-[#F3F3F3] w-[350px] rounded-xl">
                         <img
+                        className="w-[90px] h-[170px] py-3"
                             src={image}
-                            alt="Shoes" />
+                            alt="" />
                     </figure>
-                    <div className="flex mr-3">
-                        <p># <span></span></p>
-                        <p>Identity</p>
+                    <div className="flex mt-3 text-xl font-semibold text-[#23BE0A]">
+                        <p className="mr-5 px-2 py-1 bg-[#F3F3F3] rounded-2xl">#{tags1}</p>
+                        <p className="bg-[#F3F3F3] px-2 py-1 rounded-2xl">#{tags2}</p>
                     </div>
-                    <div className="border-b border-dashed">
-                        <h3>The Catcher in the Rye</h3>
-                        <p>By : Awlad Hossain</p>
+                    <div className="border-b mt-3 space-y-2 border-dashed pb-3">
+                        <h3 className="text-3xl font-semibold">{bookName}</h3>
+                        <p className="text-xl">By : {author}</p>
                     </div>
-                    <div className="flex justify-between">
-                        <div>Fiction</div>
-                        <div className="flex items-center ">
-                            <p>5.00</p>
+                    <div className="flex justify-between pt-3">
+                        <div><h4 className="text-xl">{category}</h4></div>
+                        <div className="flex text-center items-center text-xl">
+                            <p className="mr-2">{rating}</p>
                             <p><FaRegStar></FaRegStar></p>
                         </div>
+                    </div>
                     </div>
                 </div>
             </div>
